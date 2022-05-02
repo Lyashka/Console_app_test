253f6c8a678640db4dfe2ecad774b79eea51ba8a
Ila <merzlyaha@mail.ru>
2022-05-02 13:01:56 +0300

    KT2_next_step

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+"use strict";
 const numberOfFilm = +prompt('Сколько фильмов вы уже посмотретил?', '');
+
+if (numberOfFilm < 10 && numberOfFilm >= 0) {
+    alert('Просмотренно довольно мало фильмов');
+} else if (numberOfFilm > 10 && numberOfFilm < 30) {
+    alert('Вы классический зритель');
+} else if (numberOfFilm > 30) {
+    alert('Вы истинный наркоман!');
+} else {
+    alert('Упс. Произошла ошибка(');
+}
 
 const personalMoviesDB = {
     count: numberOfFilm,
@@ -8,12 +19,45 @@ const personalMoviesDB = {
     privat: false // ; не ставится, ставится, когда объект закрылся 
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+let a, b;
+for (let i = 0; i < 2; i++) {
+    a = prompt('Один из последних просмотренных фильмов?', '');
+    b = prompt('На сколько оцените его?', '');
 
-personalMoviesDB.movies[a] = b;
-personalMoviesDB.movies[c] = d;
+    while ((a.length < 5 || a.length > 15) || a.length == '' || a == null) {
+        a = prompt('Один из последних просмотренных фильмов?', '');
+        b = prompt('На сколько оцените его?', '');
+
+    }
+    personalMoviesDB.movies[a] = b;
+
+}
 
 console.log(personalMoviesDB);
+
+
+
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = +prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = +prompt('На сколько оцените его?', '');
+
+// if (b < 5){
+//     console.log("Это плохой фильм");
+//     alert(a + " Это плохой фильм");
+// }else {
+//     console.log("Это хороший фильм");
+//     alert(a + " Это хороший фильм");
+// }
+
+// if (d < 5){
+//     console.log("Это плохой фильм");
+//     alert(c + " Это плохой фильм");
+// }else {
+//     console.log("Это хороший фильм");
+//     alert(c + " Это хороший фильм");
+// }
+// personalMoviesDB.movies[a] = b;
+// personalMoviesDB.movies[c] = d;
+
+// console.log(personalMoviesDB); 
